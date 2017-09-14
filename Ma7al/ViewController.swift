6fbea36cb2ce858d70e33771399b3cfa6bb88a2d
Mustafa <mostafasanjare@yahoo.com>
2017-09-14 11:44:00 +0300

    Initial Commit

---
+++ Ma7al/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ma7al
+//
+//  Created by Mustafa on 9/14/17.
+//  Copyright © 2017 Mostafa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
